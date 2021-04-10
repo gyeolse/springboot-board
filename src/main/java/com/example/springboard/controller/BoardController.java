3c56fe508108edfedf059cedb376c703331e42f9
gyeolse <threewave@kakao.com>
2021-04-10 20:22:34 +0900

    [ADD] 게시판 조회, 수정, 삭제 구현

--- src/main/java/com/example/springboard/controller/BoardController.java
+++ src/main/java/com/example/springboard/controller/BoardController.java
@@ -4,9 +4,10 @@ import com.example.springboard.dto.BoardDto;
 import com.example.springboard.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -14,7 +15,11 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) { //Model 객체를 통해서 View에 데이터를 전달한다.
+        List<BoardDto> boardList = boardService.getBoardList();
+
+        model.addAttribute("boardList", boardList);
+
         return "board/list.html";
     }
 
@@ -26,7 +31,46 @@ public class BoardController {
     @PostMapping("/post")
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    //게시글 상세조회 페이지를 위한 컨트롤러
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+
+        return "board/detail.html";
+    }
+
+    //게시글 수정 페이지를 위한 컨트롤러
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model ) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+
+        return "board/update.html";
+    }
+
+    // 문제 해결 : https://victorydntmd.tistory.com/327?category=764331
+    //게시글 수정을 위한 컨트롤러
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
 
         return "redirect:/";
     }
+
+    // 문제 해결 : https://victorydntmd.tistory.com/327?category=764331
+    //게시글 삭제를 위한 컨트롤러
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+
 }
--- src/main/java/com/example/springboard/service/BoardService.java
+++ src/main/java/com/example/springboard/service/BoardService.java
@@ -1,19 +1,75 @@
 package com.example.springboard.service;
 
+import com.example.springboard.domain.entity.BoardEntity;
 import com.example.springboard.domain.repository.BoardRepository;
 import com.example.springboard.dto.BoardDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class BoardService {
     private BoardRepository boardRepository;
 
+    //DB에 게시글을 저장하는 서비스
     @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    //게시판의 모든 자료를 조회하는 서비스
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        //1. boardEntity 자료형의  리스트
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+
+        //2. boardDto 자료형의 List
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        //3. Conrtoller와 Service 간의 데이터 전달은 Dto로 이루어져야함.
+        //   Repository로 가져온 Entity를 반복문을 통해 Dto로 변환한 후, 전달
+        for ( BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDto);
+        }
+
+        return boardDtoList;
+    }
+
+    //게시글의 상세 정보를 조회하는 서비스
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .modifiedDate(boardEntity.getModifiedDate())
+                .build();
+
+        return boardDto;
+    }
+
+    //게시글을 삭제하는 서비스
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
     }
 }
